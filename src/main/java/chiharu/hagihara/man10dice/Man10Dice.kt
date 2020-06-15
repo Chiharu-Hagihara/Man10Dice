0a3528a005083af722c6338fa02a8e17ac690c76
M-E-C <Mr_El_Capitan@outlook.jp>
2020-06-15 21:01:59 +0900

    途中コミット！

--- src/main/java/chiharu/hagihara/man10dice/Man10Dice.kt
+++ src/main/java/chiharu/hagihara/man10dice/Man10Dice.kt
@@ -24,7 +24,6 @@ class Man10Dice : JavaPlugin() {
     override fun onDisable() {
         // Plugin shutdown logic
         config.set("radius",radius)
-        config.set("cooldown",waittime)
         this.saveConfig()
     }
 
@@ -38,55 +37,90 @@ class Man10Dice : JavaPlugin() {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         val p = sender as Player
 
-        if (args.isEmpty() || args[0] == "help") {
+        if (args.isEmpty()) {
             showHelp(p)
-        } else if (args[0] == "rangeset") {
-            var r: Int = 0
-            try {
-                r = args[1].toInt()
-            } catch (e: NumberFormatException) {
-                sender.sendMessage("$prefix§6エラーです。")
-                return false
+            return false
+        }
+        when (args[0]) {
+            "help" -> {
+                showHelp(p)
             }
-            if (r <= 0) {
-                sender.sendMessage("$prefix§c1以上で指定してください。")
-                return false
-            }
-            plugin.radius = r
-            sender.sendMessage("$prefix§a半径を${r}に設定しました。")
-        }else {
-            val put = args[0].toInt()
-            if (put <= 0) {
-                p.sendMessage(("$prefix§c１以上の数字を入力してください！"))
-                return true
-            } else if (!p.hasPermission("mdice.op")) {
-                if (waittime) {
-                    p.sendMessage(("$prefix§c現在クールタイム中です！"))
-                    return true
+
+            "rangeset" -> {
+                if (p.hasPermission("mdice.op")) {
+                    val r: Int
+                    try {
+                        r = args[1].toInt()
+                    } catch (e: NumberFormatException) {
+                        sender.sendMessage("$prefix§6エラーです。")
+                        return false
+                    }
+                    if (r <= 0) {
+                        sender.sendMessage("$prefix§c1以上で指定してください。")
+                        return false
+                    }
+                    plugin.radius = r
+                    sender.sendMessage("$prefix§a半径を${r}に設定しました。")
+                }else {
+                    p.sendMessage("$prefix §4§lYou don't have permission.")
                 }
-                waitstart()
-            } else when (args[1]) {
-                "local" -> {
+            }
+            "local" -> {
+                if (p.hasPermission("mdice.local")) {
+                    val put = args[1].toInt()
+                    if (put <= 0) {
+                        p.sendMessage(("$prefix§c１以上の数字を入力してください！"))
+                    } else if (!p.hasPermission("mdice.op")) {
+                        if (waittime) {
+                            p.sendMessage(("$prefix§c現在クールタイム中です！"))
+                        }
+                        waitstart()
+                    }
                     rollLocalDice(p, 1, put, plugin)
+                } else {
+                    p.sendMessage("$prefix §4§lYou don't have permission.")
                 }
-                "global" -> {
+            }
+            "global" -> {
+                if (p.hasPermission("mdice.global")) {
+                    val put = args[1].toInt()
+                    if (put <= 0) {
+                        p.sendMessage(("$prefix§c１以上の数字を入力してください！"))
+                    } else if (!p.hasPermission("mdice.op")) {
+                        if (waittime) {
+                            p.sendMessage(("$prefix§c現在クールタイム中です！"))
+                        }
+                        waitstart()
+                    }
                     rollGlobalDice(p, 1, put)
+                } else {
+                    p.sendMessage("$prefix §4§lYou don't have permission.")
                 }
-                else -> {
-                    rollGlobalDice(p, 1, put)
-                }
+            }
+            else -> {
+                p.sendMessage("$prefix §c§lコマンドが間違っています！")
             }
         }
         return true
     }
 
     fun showHelp(p: Player) {
-        p.sendMessage("$prefix §e=====ヘルプメニュー=====")
-        p.sendMessage("$prefix §f/mdice [数字] : ダイスを全体チャットで通知します。")
-        p.sendMessage("$prefix §f/mdice [数字] global : ダイスを全体チャットで通知します。")
-        p.sendMessage("$prefix §f/mdice local [数字] : ダイスを設定された半径のなかにいるプレイヤーに通知します。")
-        p.sendMessage("$prefix §e=====================")
-        p.sendMessage("$prefix §fCreated By Mr_El_Capitan")
+        if (p.hasPermission("mdice.local")|| p.hasPermission("mdice.global")|| p.hasPermission("mdice.op")) {
+            p.sendMessage("$prefix §e=====ヘルプメニュー=====")
+            if (p.hasPermission("mdice.local")) {
+                p.sendMessage("$prefix §f/mdice local [数字] : ダイスを設定された半径のなかにいるプレイヤーに通知します。")
+            }
+            if (p.hasPermission("mdice.global")){
+                p.sendMessage("$prefix §f/mdice global [数字] : ダイスを全体チャットで通知します。")
+            }
+            if (p.hasPermission("mdice.op")) {
+                p.sendMessage("$prefix §f/mdice rangeset [数字] : localdiceの通知範囲を設定します。")
+            }
+            p.sendMessage("$prefix §e=====================")
+            p.sendMessage("$prefix §fCreated By Mr_El_Capitan")
+        }else{
+            p.sendMessage("$prefix §4§lYou don't have permission.")
+        }
     }
 
     fun rollDice(min: Int, max: Int): Int {
@@ -121,35 +155,7 @@ class Man10Dice : JavaPlugin() {
         }
         return 0
     }
+
 }
 
-
-/**
- * [21:54:59 ERROR]: null
-org.bukkit.command.CommandException: Unhandled exception executing command 'mdice' in plugin Man10Dice v${project.version}
-at org.bukkit.command.PluginCommand.execute(PluginCommand.java:47) ~[patched_1.15.2.jar:git-Paper-126]
-at org.bukkit.command.SimpleCommandMap.dispatch(SimpleCommandMap.java:159) ~[patched_1.15.2.jar:git-Paper-126]
-at org.bukkit.craftbukkit.v1_15_R1.CraftServer.dispatchCommand(CraftServer.java:742) ~[patched_1.15.2.jar:git-Paper-126]
-at net.minecraft.server.v1_15_R1.PlayerConnection.handleCommand(PlayerConnection.java:1825) ~[patched_1.15.2.jar:git-Paper-126]
-at net.minecraft.server.v1_15_R1.PlayerConnection.a(PlayerConnection.java:1633) ~[patched_1.15.2.jar:git-Paper-126]
-at net.minecraft.server.v1_15_R1.PacketPlayInChat.a(PacketPlayInChat.java:47) ~[patched_1.15.2.jar:git-Paper-126]
-at net.minecraft.server.v1_15_R1.PacketPlayInChat.a(PacketPlayInChat.java:5) ~[patched_1.15.2.jar:git-Paper-126]
-at net.minecraft.server.v1_15_R1.PlayerConnectionUtils.lambda$ensureMainThread$0(PlayerConnectionUtils.java:23) ~[patched_1.15.2.jar:git-Paper-126]
-at net.minecraft.server.v1_15_R1.TickTask.run(SourceFile:18) ~[patched_1.15.2.jar:git-Paper-126]
-at net.minecraft.server.v1_15_R1.IAsyncTaskHandler.executeTask(IAsyncTaskHandler.java:136) ~[patched_1.15.2.jar:git-Paper-126]
-at net.minecraft.server.v1_15_R1.IAsyncTaskHandlerReentrant.executeTask(SourceFile:23) ~[patched_1.15.2.jar:git-Paper-126]
-at net.minecraft.server.v1_15_R1.IAsyncTaskHandler.executeNext(IAsyncTaskHandler.java:109) ~[patched_1.15.2.jar:git-Paper-126]
-at net.minecraft.server.v1_15_R1.MinecraftServer.ba(MinecraftServer.java:1038) ~[patched_1.15.2.jar:git-Paper-126]
-at net.minecraft.server.v1_15_R1.MinecraftServer.executeNext(MinecraftServer.java:1031) ~[patched_1.15.2.jar:git-Paper-126]
-at net.minecraft.server.v1_15_R1.IAsyncTaskHandler.awaitTasks(IAsyncTaskHandler.java:119) ~[patched_1.15.2.jar:git-Paper-126]
-at net.minecraft.server.v1_15_R1.MinecraftServer.sleepForTick(MinecraftServer.java:1015) ~[patched_1.15.2.jar:git-Paper-126]
-at net.minecraft.server.v1_15_R1.MinecraftServer.run(MinecraftServer.java:938) ~[patched_1.15.2.jar:git-Paper-126]
-at java.lang.Thread.run(Unknown Source) [?:1.8.0_241]
-Caused by: java.lang.ArrayIndexOutOfBoundsException: 0
-at chiharu.hagihara.man10dice.Man10Dice.onCommand(Man10Dice.kt:24) ~[?:?]
-at org.bukkit.command.PluginCommand.execute(PluginCommand.java:45) ~[patched_1.15.2.jar:git-Paper-126]
-... 17 more
-
- これをヌルぽと呼んでた俺がいた。
- */
 /// はっず
