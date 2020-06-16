b2e4e3e7ef5d6db7b22a1c640118a17f26ec034a
M-E-C <Mr_El_Capitan@outlook.jp>
2020-06-16 21:20:45 +0900

    途中コミット！

--- src/main/java/chiharu/hagihara/man10dice/Man10Dice.kt
+++ src/main/java/chiharu/hagihara/man10dice/Man10Dice.kt
@@ -48,6 +48,7 @@ class Man10Dice : JavaPlugin() {
                 showHelp(p)
             }
             "local" -> {
+                if(!canDice(args,sender,1))return false
                 if (p.hasPermission("mdice.local")) {
                     val put = args[1].toInt()
                         if (put <= 0) {
@@ -64,6 +65,7 @@ class Man10Dice : JavaPlugin() {
                 }
             }
             "global" -> {
+                if(!canDice(args,sender,1))return false
                 if (p.hasPermission("mdice.global")) {
                     val put = args[1].toInt()
                         if (put <= 0) {
@@ -136,6 +138,27 @@ class Man10Dice : JavaPlugin() {
             }
         }
         return 0
+    }
+
+    private fun canDice(args: Array<out String>,p: Player,start:Int):Boolean{
+        val max = start + 1
+        if (args.size != max) {
+            p.sendMessage("$prefix §c§lコマンドが間違っています！")
+            return false
+        }
+        //  正規表現
+        if(!checkNumber(args[start])){
+            p.sendMessage("$prefix §c§lコマンドが間違っています！")
+            return false
+        }
+        if (args.isEmpty()){
+            p.sendMessage("$prefix §c§lコマンドが間違っています！")
+            return false
+        }
+        return true
+    }
+    private fun checkNumber(s: String): Boolean {
+        return NumberUtils.isNumber(s)
     }
 
 }
