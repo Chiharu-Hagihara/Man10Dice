c376f1a499fc9755aadd87891cc48da69db5cca4
M-E-C <Mr_El_Capitan@outlook.jp>
2020-06-17 20:05:45 +0900

    途中コミット

--- src/main/java/chiharu/hagihara/man10dice/Man10Dice.kt
+++ src/main/java/chiharu/hagihara/man10dice/Man10Dice.kt
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit
 import net.md_5.bungee.api.ChatColor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
+import org.bukkit.command.ConsoleCommandSender
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.*
@@ -27,16 +28,20 @@ class Man10Dice : JavaPlugin() {
         val cmd = args[0]
         val cmd1 = args[1]
 
-        if (args.isEmpty()){
-            showHelp(p)
+        //コンソールからのコマンドをキャンセル
+        if (sender is ConsoleCommandSender) {
+            sender.sendMessage("This command can only be executed by a player.")
             return false
         }
 
+        if (args.isEmpty())return false
+
+        //ヘルプ表示
         if (cmd == "help"){
             showHelp(p)
-            return true
         }
 
+        //globaldice
         if (cmd == "global"){
             if (!p.hasPermission("mdice.global"))return false
             if (args.size == 1)return false
@@ -44,6 +49,7 @@ class Man10Dice : JavaPlugin() {
             GlobalDice(p, 1, put)
         }
 
+        //localdice
         if (cmd == "local"){
             if (!p.hasPermission("mdice.local"))return false
             if (args.size == 1)return false
@@ -54,27 +60,45 @@ class Man10Dice : JavaPlugin() {
         return false
     }
 
+    //グローバルダイス
     fun GlobalDice(p: Player, min: Int, max: Int): Int{
         val result = rollDice(min, max)
         Bukkit.broadcastMessage("$prefix §l${p.displayName}がダイスを振っています・・・§k§lxx")
+        //３秒間のため
         Bukkit.broadcastMessage(("$prefix §3§l${p.displayName}§3§lは§l${ChatColor.YELLOW}§l${max}§3§l面サイコロを振って${ChatColor.YELLOW}§l${result}§3§lが出た"))
         return result
     }
 
+    //ローカルダイス
     fun LocalDice(p: Player, min: Int, max: Int): Int{
         val result = rollDice(min, max)
         Bukkit.broadcastMessage("$prefix §l${p.displayName}がダイスを振っています・・・§k§lxx")
+        //３秒間のため
         Bukkit.broadcastMessage(("$prefix §3§l${p.displayName}§3§lは§l${ChatColor.YELLOW}§l${max}§3§l面サイコロを振って${ChatColor.YELLOW}§l${result}§3§lが出た"))
         return result
     }
 
+    //ダイスをふる
     fun rollDice(min: Int, max: Int): Int {
         val r = Random()
         return r.nextInt(max - min + 1) + min
     }
 
 
-    fun showHelp(p: Player){
-
+    fun showHelp(p: Player): Boolean {
+        if (p.hasPermission("mdice.local") || p.hasPermission("mdice.global") || p.hasPermission("mdice.op")) return false
+        p.sendMessage("$prefix §e=====ヘルプメニュー=====")
+        if (p.hasPermission("mdice.local")) {
+            p.sendMessage("$prefix §f/mdice local [数字] : ダイスを設定された半径のなかにいるプレイヤーに通知します。")
+        }
+        if (p.hasPermission("mdice.global")) {
+            p.sendMessage("$prefix §f/mdice global [数字] : ダイスを全体チャットで通知します。")
+        }
+        if (p.hasPermission("mdice.op")) {
+            p.sendMessage("$prefix §f/mdice rangeset [数字] : localdiceの通知範囲を設定します。")
+        }
+        p.sendMessage("$prefix §e=====================")
+        p.sendMessage("$prefix §fCreated By Mr_El_Capitan")
+        return true
     }
 }
