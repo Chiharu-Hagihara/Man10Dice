7ebb9f00104699f48690459128319d42d9819de4
M-E-C <Mr_El_Capitan@outlook.jp>
2020-06-17 21:32:28 +0900

    コミット

--- src/main/java/chiharu/hagihara/man10dice/Man10Dice.kt
+++ src/main/java/chiharu/hagihara/man10dice/Man10Dice.kt
@@ -109,7 +109,12 @@ class Man10Dice : JavaPlugin() {
         waittime = true
         object: BukkitRunnable(){
             override fun run(){
-                Bukkit.broadcastMessage(("$prefix §3§l${p.displayName}§3§lは§l${ChatColor.YELLOW}§l${max}§3§l面サイコロを振って${ChatColor.YELLOW}§l${result}§3§lが出た"))
+                p.sendMessage(("$prefix §3§l${p.displayName}§3§lは§l${ChatColor.YELLOW}§l${max}§3§l面サイコロを振って${ChatColor.YELLOW}§l${result}§3§lが出た"))
+                for (players in p.getNearbyEntities(plugin.radius.toDouble(), plugin.radius.toDouble(), plugin.radius.toDouble())) {
+                    if (players is Player) {
+                        p.sendMessage(("$prefix §3§l${p.displayName}§3§lは§l${ChatColor.YELLOW}§l${max}§3§l面サイコロを振って${ChatColor.YELLOW}§l${result}§3§lが出た"))
+                    }
+                }
                 waittime = false
             }
         }.runTaskLater(plugin,20*3)
